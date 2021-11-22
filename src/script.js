2e9df1e856251b5da0b86b77e4c3760ea6650c51
Viktor <f49411151@gmail.com>
2021-11-22 13:45:58 +0300

    function declaration | function expression

--- src/script.js
+++ src/script.js
@@ -1,19 +1,26 @@
 'use strict';
 
-/**
- * функция возвращает скидку 
- * @param {number} величина скидки, максимум 30%. По умолчанию 10%.
- * @returns {number}
- */
-function setDiscount(discount = 10) {
-    if (discount > 30) {
-        return 30;
-    }
-    return discount;
-}
 
 let order1Discount = setDiscount(15);
 alert(order1Discount);
 
 let order2Discount = setDiscount();
 alert(order2Discount);
+
+/* function declaration вызвать можно до объявления самой функции */
+function setDiscount(discount = 10) {
+    if (discount > 30) {
+        return 30;
+    }
+    return discount;
+    /* после фигурных скобок точка с запятой не обязательна */
+}
+
+
+/* function expression или анонимная функция (без имени) вызвать можно только после объявления */
+let getSquare = function(x) {
+    return x * x;
+    /* точка с запятой после скобок обязательна */
+};
+
+console.log(getSquare(3));
