f1d93c60551a1ea50aa362b056a84f75f1b03166
Viktor <f49411151@gmail.com>
2021-11-22 17:46:41 +0300

    script generates password | генератор пароля

--- src/script.js
+++ src/script.js
@@ -1,16 +1,42 @@
 'use strict';
+
+let symbols = '1234567890!@#$%^&*()-_=+;:][}{/?.,qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCBNM';
+let password = '';
+
 /**
- * рекурсия. функция вызывает саму себя
- * @param {*} n 
- * @returns 
+ * Функция возвращает случайное целое число между min (включительно) и max (не включая max)
+ * @param {*} min 
+ * @param {*} max 
+ * @returns {number}
  */
-const factorial = (n) => {
-    if (n === 1) {
-        return 1;
-    } else {
-        return n * factorial(n-1);
+function getRandomInt(min, max) {
+    /* .random - выбор случайного числа .floor - отбрасывает дробную часть */
+    return Math.floor(Math.random() * (max - min)) + min; 
+}
+
+/**
+ * Функция генерирует случайный пароль
+ * @param {number} passLength длина пароля
+ * @returns {void}
+ */
+function generate(passLength) {
+    let symbolPosition = getRandomInt(0, 87); // массив от 0 до 86. последнее число не включается
+    password += symbols.charAt(symbolPosition); // += дописывает символ в массив
+    passLength--; // -- уменьшает на единицу
+    if (passLength != 0) {
+        generate(passLength);
     }
 }
 
-const answer = factorial(3);
-console.log(answer);
+/**
+ * Функция запрашивает длину пароля
+ * @returns {number}
+ */
+function askPassLength() {
+    return parseInt(prompt('Введите длину пароля'));
+}
+
+/* Вызываем функцию generate и передаем в нее значение функции 
+askPassLength() - эта запись тоже вызов функции askPassLength */
+generate(askPassLength());
+alert(`Ваш пароль: ${password}`);
