d3d39a08e4a0874578b0838caa75bd9b5d0278a6
Viktor <f49411151@gmail.com>
2021-11-22 13:22:31 +0300

    function contains value, takes value, returns result

--- src/script.js
+++ src/script.js
@@ -1,31 +1,19 @@
 'use strict';
 
-function roundToHundredths(number) {
-    /* parseInt отбрасывает дробную часть */
-    return parseInt(number * 100) / 100;
-}
-
-/* объявляем функцию, (roudles, currency) - параметры функции*/
-function convertCurrency(roubles, currency) {
-    let currencyCost = 0;
-    switch (currency) {
-        case 'dollar':
-            currencyCost = 66;
-            break;
-        case 'euro':
-            currencyCost = 75;
-            break;
-        default: 
-        /* оператор throw выбрасывает ошибки, new - оператор создания объекта, Error - объект ошибок */
-        throw new Error('Не верная валюта: ' + currency);
+/**
+ * функция возвращает скидку 
+ * @param {number} величина скидки, максимум 30%. По умолчанию 10%.
+ * @returns {number}
+ */
+function setDiscount(discount = 10) {
+    if (discount > 30) {
+        return 30;
     }
-    return roundToHundredths(roubles / currencyCost);
+    return discount;
 }
 
-/* вызываем функцию и передаем в нее аргументы (число 100 и строка 'dollar') - аргументы которые передаем в функцию */
-let inDollars = convertCurrency(100, 'dollar');
-console.log('Ваша сумма в долларах: ' + inDollars);
-let inEurus = convertCurrency(100, 'euro');
-console.log('Ваша сумма в евро: ' + inEurus);
-let inYens = convertCurrency(100, 'yena');
-console.log('Ваша сумма в йенах: ' + inYens);
+let order1Discount = setDiscount(15);
+alert(order1Discount);
+
+let order2Discount = setDiscount();
+alert(order2Discount);
