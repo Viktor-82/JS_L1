655485e7048487da7eef16748c13a7f930f5d451
Viktor <f49411151@gmail.com>
2021-11-23 18:14:00 +0300

    .sort() | метод сортировки без создания нового массива

--- src/script.js
+++ src/script.js
@@ -1,6 +1,27 @@
 'use strict';
 
-/* Посмотреть собственные свойства объекта для чтобы узнать все его методы.*/
-const array = [9, 3, 4];
+/* Метод sort() сортирует массив не выводя новый. 
+Т.е. преобразует его прямо на месте. Порядок сортировки с
+соответствует порядковому номеру в таблице  Unicode
+url: unicode-table.com/ru/
+ */
 
-console.log(Object.getOwnPropertyNames(array));// 0, 1, 2, 3, length
+let values = ['ad', 'ae', 'ab', 'aa', 'ac'];
+values.sort();
+console.log(values);// 'aa', 'ab', 'ac', 'ae', 'ad'
+
+/* Передаем в метод sort() функцию.
+Массив будет отсортирован по длинне слов от меньшей к большей
+*/
+let value = ['one', 'left', 'rightqqqqq', 'or'];
+value.sort(function(word1, word2) {
+    if (word2.length > word1.length) {
+        return -1;
+    } 
+    if (word1.length > word2.length) {
+        return 1;
+    }
+        return 0;       
+});
+
+console.log(value);
