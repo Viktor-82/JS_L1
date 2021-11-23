ba03fade165e3c6aa74b2b3965dc637d6c7d0dd3
Viktor <f49411151@gmail.com>
2021-11-23 15:49:02 +0300

    do/while | цикл выполняется до проверки условий

--- src/script.js
+++ src/script.js
@@ -1,30 +1,9 @@
 'use strict';
 
-const user = {
-    id: 3,
-    age: 25,
-    name: 'John'
-};
-
-/* for / in предназначен для объектов.
- Цикл for/in по умолчанию будет перебирать и возвращать ключи элементов 
-(по другому свойства элементов) 
-*/
-for (let property in user) {
-    console.log(property);// id, age, name
-};
-
-/* При такой записи цикл for/in будет перебирать и возвращать значения ключей 
-*/
-for (let property in user) {
-    console.log(user[property]);// 3, 25, 'John'
-}
-
-/* for / of предназначен для массивов. 
-Перебирает только значения
-*/
-let array = ['one', 'two', 'tree'];
-
-for (let value of array) {
-    console.log(value);
-}
+/* Цикл do/while сначала выполняется потом проверяет условия
+поэтому выполнится всегда хотя бы один раз */
+let i = 0;
+do {
+    console.log('Счетчик равен: ' + i);
+    i++;
+} while (i < -1);
