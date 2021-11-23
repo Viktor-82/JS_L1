1e8cb98f9e459c96a3cd786a2649c6f15df79e0a
Viktor <f49411151@gmail.com>
2021-11-23 11:33:17 +0300

    forEach for iterate array | метод forEach для перебора массива

--- src/script.js
+++ src/script.js
@@ -1,10 +1,28 @@
 'use strict';
 
-/* Оператор in используется для проверки есть ли в объекте какое то свойство */
-const user = {
-    name: 'John',
-    age: 36
+let images = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg'
+];
+
+/* Метод forEach для перебора массива.
+Массивы создаются на основе объекта Array а у объекта есть методы один из которых forEach 
+*/
+images.forEach(function(element) {
+    console.log(element + ' hello');
+});
+
+/* У function стандартно есть 3 параметра. Последние 2 необязательны поэтому часто не пишутся */
+images.forEach(function(element, index, array) {
+    console.log(element, index, array);// элемент, номер элемента и сам массив (передача по ссылке)
+});
+
+
+/* Пример передачи ссылки на что либо */
+const iterateFn = function (element, index, array) {
+    console.log(element, index, array);
 };
 
-console.log('age' in user);// true
-console.log('surname' in user);// false
+/* В forEach передается ссылка на функцию которая хранится в переменной */
+images.forEach(iterateFn);
