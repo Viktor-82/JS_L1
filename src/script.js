d4079c701f28ebe5bf1050e3a895b69f918b60f5
Viktor <f49411151@gmail.com>
2021-11-23 11:39:22 +0300

    method pop | метод pop удаление последнего элемента массива

--- src/script.js
+++ src/script.js
@@ -1,28 +1,10 @@
 'use strict';
 
-let images = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg'
-];
+let arr = ['hello', 'world', '!'];
 
-/* Метод forEach для перебора массива.
-Массивы создаются на основе объекта Array а у объекта есть методы один из которых forEach 
-*/
-images.forEach(function(element) {
-    console.log(element + ' hello');
-});
-
-/* У function стандартно есть 3 параметра. Последние 2 необязательны поэтому часто не пишутся */
-images.forEach(function(element, index, array) {
-    console.log(element, index, array);// элемент, номер элемента и сам массив (передача по ссылке)
-});
-
-
-/* Пример передачи ссылки на что либо */
-const iterateFn = function (element, index, array) {
-    console.log(element, index, array);
-};
-
-/* В forEach передается ссылка на функцию которая хранится в переменной */
-images.forEach(iterateFn);
+/* Метод pop() удаляет последний элемент из массива 
+и возвращает его значение
+ */
+let last = arr.pop();
+console.log(last);// '!'
+console.log(arr);// ['hello', 'world']
