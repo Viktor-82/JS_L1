f9ffbe2523526da83307f1254bb754cf74185e64
Viktor <f49411151@gmail.com>
2021-11-23 11:58:25 +0300

    method unshift() | метод unshift() добавляет элемент(ы) в начало массива

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let images = ['1.jpg', '2.jpg', '3.png'];
+let userId = ['1651', '8944444', '231'];
 
-/* Метод shift() удаляет первый элемент из массива и 
-возвращает его значение. Соответственно длина массива меняется.
+/* Метод unshift() добавляет один или более элементов
+в начало массива и возвращает новую длину массива.
  */
-let firstElem = images.shift();
-console.log(firstElem);// 1.jpg
-console.log(images);// ['2.jpg', '3.png']
+let newArrLength = userId.unshift('11111');
+console.log(newArrLength);// 4
+console.log(userId);// ['11111', '1651', '8944444', '231']
