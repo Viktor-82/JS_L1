3b49057026f75ce3c399e7f2f0e32dba573b1085
Viktor <f49411151@gmail.com>
2021-11-23 17:24:24 +0300

    поспотреть собственные свойства объекта

--- src/script.js
+++ src/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
-/* Посмотреть прототип объекта для чтобы узнать 
-все его методы.*/
-const myArr = [9, 3, 4];
-/* Разные формы записи выполняют одно и тоже */
-console.log(myArr.__proto__);// современная запись
-console.log(Object.getPrototypeOf(myArr));// для всех браузеров в т.ч. старых
+/* Посмотреть собственные свойства объекта для чтобы узнать все его методы.*/
+const array = [9, 3, 4];
+
+console.log(Object.getOwnPropertyNames(array));// 0, 1, 2, 3, length
