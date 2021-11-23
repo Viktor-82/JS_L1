23023c99184f79f3c53eba5cbea5282356c2a283
Viktor <f49411151@gmail.com>
2021-11-23 11:03:28 +0300

    operator in | оператор in для проверки наличия свойств в объекте

--- src/script.js
+++ src/script.js
@@ -1,20 +1,10 @@
 'use strict';
 
-/* Перебор массива циклом for */
-const arr = [5, 7, 9];
-for (let i =0; i < arr.length; i++) {
-    alert(arr[i]);
-}
+/* Оператор in используется для проверки есть ли в объекте какое то свойство */
+const user = {
+    name: 'John',
+    age: 36
+};
 
-/* Вложенный цикл for перебор массива массивов*/
-let arrNum = [
-    [5, 7, 9],
-    [2, 0, 2, 12, 34],
-    [1, 3, 4, 6]
-];
-
-for (let i = 0; i < arrNum.length; i++) {
-    for (let j = 0; j < arrNum[i].length; j++) {
-        console.log(arrNum[i][j]);
-    }
-}
+console.log('age' in user);// true
+console.log('surname' in user);// false
