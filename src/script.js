f015bc16fc80494ee61c894476794fb588f42144
Viktor <f49411151@gmail.com>
2021-11-24 18:50:05 +0300

    свойство this

--- src/script.js
+++ src/script.js
@@ -1,14 +1,13 @@
 'use strict';
 
-/* Метод sort() сортирует массив не выводя новый. 
-Т.е. преобразует его прямо на месте. Порядок сортировки с
-соответствует порядковому номеру в таблице  Unicode
-url: unicode-table.com/ru/
- */
+const obj1 = {
+    author: 'User',
+    text: 'hello world',
+    show: function() {
+        alert(this.author + ' say: ' + this.text);// this указывает на сам объект
+    }
+};
 
-/* Короткий вариант записи сортировки если значения цифровые*/
-let sortActualProducts = actualProducts.sort(function(item1, item2){
-    return item1.price - item2.price;
-});
-
-console.log(actualProducts); 
+console.log(obj1.author);
+console.log(obj1.text);
+obj1.show();// вызов метода объекта
