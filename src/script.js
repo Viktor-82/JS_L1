0ab4985b33a2c6c899f186113d20891472c71ec4
Viktor <f49411151@gmail.com>
2021-11-24 20:58:18 +0300

    функция конструктор с использованием другого конструктора

--- src/script.js
+++ src/script.js
@@ -1,25 +1,23 @@
 'use strict';
 
-/* Создание функции конструктора. Название конструктора всегда с большой буквы. */
-function Post(author, text) {
-    //этот this будет указывать на вновь создаваемый объект
-    this.author = author;
-    this.text = text;
-}
-/* Запись метода конструктора объектов нужно делать не в сам конструктор а в его прототип,
- чтобы не расходовать оперативную память при создании каждого нового объекта из конструктора.
- Все вновь созданные объекты будут по ссылке из одной и той же области оперативной памяти получать метод. */
-Post.prototype.show = function() {
-    alert(this.author + ' say: ' + this.text);
+/* Создание функции конструктора с использованием
+другой существующей функции конструктора
+ */
+function UserRole() {
+    this.rights = ['create', 'edit'];
+    // this.__proto__ = UserRole.prototype
 }
 
-/* Создание нового объекта на основе конструктора начинается с ключевого слова new */
-const post1 = new Post('Admin', 'lorem ipsum1');
-console.log(post1.author);
-console.log(post1.text);
-post1.show();
-
-const post2 = new Post('User', 'hello world');
-console.log(post2.author);
-console.log(post2.text);
-post2.show();
+function ModerRole() {
+    UserRole.call(this);
+    this.rights.push('delete');
+    // this.__proto__ = ModerRole.prototype
+}
+/* Та же функция в другой записи
+function ModerRole() {
+    let hello = this;
+    UserRole.call(hello);
+    this.rights.push('delete');
+}
+ */
+const moderRole1 = new ModerRole();
