a22572ca1a82a139b3f6c41aaa4341bf304e1f9c
Viktor <f49411151@gmail.com>
2021-11-24 19:20:48 +0300

    функция конструктор объектов

--- src/script.js
+++ src/script.js
@@ -1,13 +1,25 @@
 'use strict';
 
-const obj1 = {
-    author: 'User',
-    text: 'hello world',
-    show: function() {
-        alert(this.author + ' say: ' + this.text);// this указывает на сам объект
-    }
-};
+/* Создание функции конструктора. Название конструктора всегда с большой буквы. */
+function Post(author, text) {
+    //этот this будет указывать на вновь создаваемый объект
+    this.author = author;
+    this.text = text;
+}
+/* Запись метода конструктора объектов нужно делать не в сам конструктор а в его прототип,
+ чтобы не расходовать оперативную память при создании каждого нового объекта из конструктора.
+ Все вновь созданные объекты будут по ссылке из одной и той же области оперативной памяти получать метод. */
+Post.prototype.show = function() {
+    alert(this.author + ' say: ' + this.text);
+}
 
-console.log(obj1.author);
-console.log(obj1.text);
-obj1.show();// вызов метода объекта
+/* Создание нового объекта на основе конструктора начинается с ключевого слова new */
+const post1 = new Post('Admin', 'lorem ipsum1');
+console.log(post1.author);
+console.log(post1.text);
+post1.show();
+
+const post2 = new Post('User', 'hello world');
+console.log(post2.author);
+console.log(post2.text);
+post2.show();
