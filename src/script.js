e82399a3e6fe74356b4e7f2aed40f1d9ec42fcb0
Viktor <f49411151@gmail.com>
2021-11-25 09:38:55 +0300

    сеттер / ограничения!

--- src/script.js
+++ src/script.js
@@ -1,20 +1,33 @@
 'use strict';
 
 class User {
-    constructor(name, surname) {
+    url = null;
+    constructor(name, age) {
         this.name = name;
-        this.surname = surname;
+        this.age = age;
     }
-    /* Геттер возвращает значение указанное в исполнительном блоке */
-    get fullName() {
-        return this.name + ' ' + this.surname;
+    /* Сеттер для передачи данных в конструктор объекта*/
+    set vkUrl(url) {
+        if (/^https?.*/gm.test(url)) {
+            this.url = url;
+        } else {
+            throw new Error('Passed invalid url value: ' + url);
+        }
     }
 }
 
-const user1 = new User('Alex', 'Smith'); //В конструктор можно передать значения
-console.log(user1.name);
-console.log(user1.surname);
-console.log(user1.fullName);// обращение к геттеру как к обычном свойству
+const user1 = new User('Alice', 25);
+/* Обращение к сеттеру как к свойству хотя такого свойства нет */
+user1.vkUrl = 'https://vk.com/test'; 
 
-/* В геттер нельзя ничего передать! Это не функция */
-/* user1.fullName = 'John Doe'; */ // Такая запись покажет ошибку!!
+/* При записи ниже будет ошибка т.к. такие данные 
+не пройдут проверки регулярным выражением */
+user1.vkUrl = 'hello world';
+
+/* Ограничение сеттера. Нельзя использовать такую запись
+ сеттера в конструкторе! */
+ /* set name(userName) {
+     this.name = userName; // будет выполять операцию по кругу пока не забъет всю память!
+ } */
+
+
