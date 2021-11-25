7e94f0283800b1e88ac9922bd448c4854cce60c4
Viktor <f49411151@gmail.com>
2021-11-25 10:45:49 +0300

    статические свойства и методы

--- src/script.js
+++ src/script.js
@@ -1,11 +1,41 @@
 'use strict';
 
-function User() {}
+/* Статические методы присваиваются не в prototype функции а в саму функцию.
+Статические методы и свойства также наследуются.
+Они принадлежат классу а не конкретному объекту и вызываются
+через имя класса */
+function User(name) {
+    this.name = name;
+}
+/* Статическое свойство конструктора */
+User.nameMaxLength = 20; 
 
-const user1 = new User();
+/* Статический метод конструктора */
+User.isNameLengthCorrect = function (name) {
+    if (name.length <= User.nameMaxLength) {
+        console.log('Name is correct.');
+    } else {
+        console.log('Name is incorrect.');
+    }
+};
 
-/* Определяет является ли user1 
-экземпляром конструктора User */
-console.log(user1 instanceof User); // будет true
+const user1 = new User('John');
+User.isNameLengthCorrect(user1.name);
 
+/* Тот же код в синтаксисе ES6 */
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+    static nameMaxLength = 20;
 
+    static isNameLengthCorrect(name) {
+        if (name.length <= User.nameMaxLength) {
+            console.log('Name is correct.');
+        } else {
+            console.log('Name is incorrect.');
+        }
+    }
+}
+const user1 = new User('John');
+User.isNameLengthCorrect(user1.name); // вызов через имя класса а не конкретного объекта
