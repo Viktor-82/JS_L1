b1c6d44326576a7032559b024ac62d5a7d2ddfc5
Viktor <f49411151@gmail.com>
2021-11-25 10:10:57 +0300

    isctanceof | является ли экземпляром конструктора

--- src/script.js
+++ src/script.js
@@ -1,33 +1,11 @@
 'use strict';
 
-class User {
-    url = null;
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    /* Сеттер для передачи данных в конструктор объекта*/
-    set vkUrl(url) {
-        if (/^https?.*/gm.test(url)) {
-            this.url = url;
-        } else {
-            throw new Error('Passed invalid url value: ' + url);
-        }
-    }
-}
+function User() {}
 
-const user1 = new User('Alice', 25);
-/* Обращение к сеттеру как к свойству хотя такого свойства нет */
-user1.vkUrl = 'https://vk.com/test'; 
+const user1 = new User();
 
-/* При записи ниже будет ошибка т.к. такие данные 
-не пройдут проверки регулярным выражением */
-user1.vkUrl = 'hello world';
-
-/* Ограничение сеттера. Нельзя использовать такую запись
- сеттера в конструкторе! */
- /* set name(userName) {
-     this.name = userName; // будет выполять операцию по кругу пока не забъет всю память!
- } */
+/* Определяет является ли user1 
+экземпляром конструктора User */
+console.log(user1 instanceof User); // будет true
 
 
