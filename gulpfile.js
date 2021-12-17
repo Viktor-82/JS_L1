11b9363132f820c765bfbac2091842072ac95acf
Viktor <f49411151@gmail.com>
2021-12-17 19:05:16 +0300

    игра змейка

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,16 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('server', function() {
+function buildJs() {
+    return gulp.src('src/*.js') // это папка где лежат js файлы
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.js')) // это как будет называться собранный файл
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('dist/')); // папка в которую будем складывать файл
+}
+/* Следит за всеми файлами в src/ с расширением .js и 
+применяет к ним в случае изменение функцию buildJs */
+gulp.watch('src/*.js', buildJs); 
 
-    browserSync({
-        server: {
-            baseDir: "src"
-        }
-    });
-
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
-
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
-
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+exports.default = buildJs; 
